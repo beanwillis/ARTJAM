8074d1ec9e8226f342be2d2c19f84c7a89692c60
Cindy <zxx.njc@gmail.com>
2020-10-08 02:23:55 +0800

    skill, issue and interest selection pages

--- templates/wheel2.php
+++ templates/wheel2.php
@@ -1,53 +1,77 @@
 <?php
+    session_start();
     if (!isset( $_GET['interest'])) {
         header('Location: wheel1.html');
     } else {
         $interest = $_GET['interest'];
+        $_SESSION['interest'] = $interest;
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="wheel.css">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <link rel="stylesheet" href="progress-tracker.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
-        <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+<body style="background-color: #fdf6ee;">
+    <style>
+        .btn-secondary:focus {
+            background-color: #ea755a !important;
+            color: white;
+            box-shadow: none!important;
+            border: none;
+        }
+    </style>
+    
+    <div class="container mt-4">
+        <span class="glyphicon glyphicon-home" style="font-size:20px;"></span>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <p class="lead text-center mt-3">Please select a <strong>skill</strong> that is most relevant to you.</p>
+        <form action="wheel3.php" method="get">
+            <div class="btn-group-vertical mt-10" style="width:100%">
+                <button id="social" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Social</h4>
+                    <p class="font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="creative" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Creative</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="business" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Business</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="leadership" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Leadership</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="technology" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Technology</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+            </div>
+        </form>
+        <button type="button" class="btn btn-default btn-sm" style="font-size: 50px;background-color: #fdf6ee;border: none; width:70px; height:70px; border-radius:50%;">
+            <a href="wheel1.html" style="color:inherit"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>
+        </button>
     </div>
-    <p class="header text-center">Please select a <strong>skill</strong> that is most relevant to you.</p>
-    <form action="wheel3.php" method="get">
-        <div class="container">
-            <img src="../static/img/social.png" alt="Social">
-            <button name="skill" value="social" onclick="window.location.href='wheel3.php';" class="btn" style="background-color: #91e2f0;color: black;">Social</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/creative.jpg" alt="Creative">
-            <button name="skill" value="creative" onclick="window.location.href='wheel3.php';" class="btn" style="background-color: #fbd743;color: black;">Creative</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/business.jpeg" alt="Business">
-            <button name="skill" value="business" onclick="window.location.href='wheel3.php';" class="btn" style="background-color: #edbc84;color: black;">Business</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/leadership.png" alt="Leadership">
-            <button name="skill" value="leadership" onclick="window.location.href='wheel3.php';" class="btn" style="background-color: #99c1dc;color: black;">Leadership</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/technology.jpeg" alt="Technology">
-            <button name="skill" value="technology" onclick="window.location.href='wheel3.php';" class="btn" style="background-color: #074763;color: white;">Technology</button>
-        </div>
-        <input type="hidden" name="interest" value="<?php echo $interest ?>">
-    </form>
-    <a href="wheel1.html" class="previous">&#8249;</a>
-    <!-- <a href="wheel3.php" class="next">&#8250;</a> -->
+
+    <script>
+        function selection(clicked) {
+            // var url = "wheel3.php?skill=" + clicked;
+            // document.location.href = url;
+            sessionStorage.setItem("skill", clicked);
+        }
+    </script>
 </body>
 </html>
--- templates/wheel3.php
+++ templates/wheel3.php
@@ -1,56 +1,79 @@
 <?php
-    if (!isset( $_GET['skill']) && isset($_GET['interest'])) {
-        header('Location: wheel2.html');
+    session_start();
+    $interest = $_SESSION['interest'];
+    if (!isset( $_GET['skill'])) {
+        header('Location: wheel2.php');
     } else {
         $skill = $_GET['skill'];
-        $interest = $_GET['interest'];
+        $_SESSION['skill'] = $skill;
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="wheel.css">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <link rel="stylesheet" href="progress-tracker.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
-        <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-        <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+<body style="background-color: #fdf6ee;">
+    <style>
+        .btn-secondary:focus {
+            background-color: #ea755a !important;
+            color: white;
+            box-shadow: none!important;
+            border: none;
+        }
+    </style>
+    
+    <div class="container mt-4">
+        <span class="glyphicon glyphicon-home" style="font-size:20px;"></span>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <p class="lead text-center mt-3">Please select an <strong>issue</strong> that is most relevant to you.</p>
+        <form action="wheel3.php" method="get">
+            <div class="btn-group-vertical mt-10" style="width:100%">
+                <button id="education" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Education & Mobility</h4>
+                    <p class="font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="equality" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Equality & Diversity</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="environment" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Environment & Sustainability</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="health" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Health & Wellbeing</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+                <button id="cybersecurity" onClick="selection(this.id)" type="button" class="btn btn-secondary mb-3" style="background-color: #4a8bae;white-space: normal;">
+                    <h4 class="display-5">Cybersecurity</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </button>
+            </div>
+        </form>
+        <button type="button" class="btn btn-default btn-sm" style="font-size: 50px;background-color: #fdf6ee;border: none; width:70px; height:70px; border-radius:50%;">
+            <a href="wheel2.php?interest=<?php echo $interest; ?>" style="color:inherit"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>
+        </button>
     </div>
-    <p class=" header text-center">Please select an <strong>issue</strong> that is most relevant to you.</p>
-    <form action="final.php" method="get">
-        <div class="container">
-            <img src="../static/img/education.jpg" alt="Education & Mobility">
-            <button name="issue" value="education-mobility" onclick="window.location.href='final.php';" class="btn" style="background-color: #faeac6;color: black;">Education & Mobility</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/equality.jpg" alt="Equality & Diversity">
-            <button name="issue" value="equality-diversity" onclick="window.location.href='final.php';" class="btn" style="background-color: #ffdfa2;color: black;">Equality & Diversity</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/environment.jpg" alt="Environment & Sustainability">
-            <button name="issue" value="environment-sustainability" onclick="window.location.href='final.php';" class="btn" style="background-color: #1f8e26;color: white;">Environment & Sustainability</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/health-wellbeing.jpg" alt="Health & Wellbeing">
-            <button name="issue" value="health-wellbeing" class="btn" style="background-color: #d1cabf;color: black;">Health & Wellbeing</button>
-        </div>
-        <div class="container">
-            <img src="../static/img/cybresecurity.jpg" alt="Cybersecurity">
-            <button name="issue" value="cybersecurity" class="btn" style="background-color: #0a3853;color: white;">Cybersecurity</button>
-        </div>
-        <input type="hidden" name="interest" value="<?php echo $interest ?>">
-        <input type="hidden" name="skill" value="<?php echo $skill ?>">
-    </form>
-    <a href="wheel2.php" class="previous">&#8249;</a>
-    <!-- <a href="final.php" class="next">&#8250;</a> -->
+
+    <script>
+        function selection(clicked) {
+            // var url = "wheel3.php?issue=" + clicked;
+            // document.location.href = url;
+            sessionStorage.setItem("issue", clicked);
+        }
+    </script>
 </body>
 </html>
