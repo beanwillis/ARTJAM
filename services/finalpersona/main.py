af97e94239942f35250c35939545283323850eef
tammi <tammichng.co@gmail.com>
2020-10-08 14:28:20 +0800

    Update main.py

--- services/finalpersona/main.py
+++ services/finalpersona/main.py
@@ -31,13 +31,27 @@ def get_all_final_personas():
 
     persona_list = []
     for doc in docs:
-        print(doc)
         persona_list.append(doc.to_dict())
     
     try:
         return {
             'final personas': persona_list
         }, 200
+    except Exception as e:
+        print(e)
+        return {"error": "Cannot retrieve final personas"}, 500
+
+@app.route("/get_final_persona/<string:interimPersona>/<string:likeTo>")
+def get_final_persona(likeTo, interimPersona):
+    final_ref = db.collection(u"FinalPersona")
+    docs = final_ref.where("likeTo", "==", likeTo).where("interimPersona", "==", interimPersona).stream()
+    
+    try:
+        for doc in docs:
+            return {
+                'data': doc.to_dict()
+            }, 200
+        
     except Exception as e:
         print(e)
         return {"error": "Cannot retrieve final personas"}, 500
@@ -59,9 +73,41 @@ def get_all_books():
         print(e)
         return {"error": "Cannot retrieve books"}, 500
 
+@app.route("/get_books_by_final_persona/<string:finalPersona>")
+def get_books_by_final_persona(finalPersona):
+
+    persona_ref = db.collection(u"FinalPersona")
+    persona_docs = persona_ref.where("finalPersona", "==", finalPersona).stream()
+
+    final_ref = db.collection(u"Books")
+
+    books = ""
+    try:
+        for doc in persona_docs:
+            books = doc.to_dict()["books"]
+
+        book_list = books.split(",")
+        for b in book_list:
+            b.replace("\\n", " ")
+
+        book_details = []
+
+        for b in book_list:
+            book_docs = final_ref.where("name", "==", b).stream()
+            for b_doc in book_docs:
+                book_details.append(b_doc.to_dict())
+
+        return {
+            "books" : book_details
+        }, 200
+    except Exception as e:
+        print(e)
+        return {"error" : "Cannot retrieve book"}, 500
+
+
 @app.route("/get_all_skills")
 def get_all_skills():
-    final_ref = db.collection(u"FinalPersona")
+    final_ref = db.collection(u"Skills")
     docs = final_ref.stream()
 
     persona_list = []
@@ -71,11 +117,39 @@ def get_all_skills():
     
     try:
         return {
-            'final personas': persona_list
+            'skills': persona_list
         }, 200
     except Exception as e:
         print(e)
-        return {"error": "Cannot retrieve final personas"}, 500
+        return {"error": "Cannot retrieve skills"}, 500
+
+@app.route("/get_skills_by_final_persona/<string:finalPersona>")
+def get_skills_by_final_persona(finalPersona):
+
+    persona_ref = db.collection(u"FinalPersona")
+    persona_docs = persona_ref.where("finalPersona", "==", finalPersona).stream()
+
+    final_ref = db.collection(u"Skills")
+
+    skills = ""
+    try:
+        for doc in persona_docs:
+            skills = doc.to_dict()["skills"]
+
+        skill_list = skills.split(",")
+        skill_details = []
+        for b in skill_list:
+            skill_docs = final_ref.where("Skills", "==", b).stream()
+            for b_doc in skill_docs:
+                skill_details.append(b_doc.to_dict())
+
+        return {
+            "skills" : skill_details
+        }, 200
+    except Exception as e:
+        print(e)
+        return {"error" : "Cannot retrieve book"}, 500
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
